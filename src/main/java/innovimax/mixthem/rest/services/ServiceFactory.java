eda85b7170d672f95e213f82ae92717552297fcf
Innovimax team <kojimch@gmail.com>
2017-03-17 12:35:17 +0100

    Create ServiceFactory.java

---
+++ src/main/java/innovimax/mixthem/rest/services/ServiceFactory.java
@@ -0,0 +1,19 @@
+package innovimax.mixthem.rest.services;
+
+public class ServiceFactory {
+
+    // TODO
+    //private final static TextService textService = new TextServiceImpl();
+    //private final static FileService fileService = new FileServiceImpl();
+
+    public static TextService getTextService() {
+        //return textService;
+        return null;
+    }
+
+    public static FileService getFileService() {
+        //return fileService;
+        return null;
+    }
+
+}
