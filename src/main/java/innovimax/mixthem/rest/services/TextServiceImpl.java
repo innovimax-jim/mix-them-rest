98a631eccd14be6840df2bef4ee67c1d08986b42
Innovimax team <kojimch@gmail.com>
2017-03-20 10:23:50 +0100

    Update TextServiceImpl.java

--- src/main/java/innovimax/mixthem/rest/services/TextServiceImpl.java
+++ src/main/java/innovimax/mixthem/rest/services/TextServiceImpl.java
@@ -1,32 +1,52 @@
 package innovimax.mixthem.rest.services;
 
+import innovimax.mixthem.rest.request.CheckFactory;
+import innovimax.mixthem.rest.request.TextParamsCheck;
+
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 public class TextServiceImpl implements TextService {
 
+    private final TextParamsCheck check = CheckFactory.getTextParamsCheck();
+
     @Override
     public Response addFromText(String text1, String text2) {
+        if (!check.addFromText(text1, text2)) {
+            return check.getErrorResponse();
+        }        
         return Response.status(Status.NOT_IMPLEMENTED).build();
     }
 
     @Override
     public Response altFromText(String type, String text1, String text2) {
+        if (!check.altFromText(type, text1, text2)) {
+            return check.getErrorResponse();        
+        } 
         return Response.status(Status.NOT_IMPLEMENTED).build();
     }
 
     @Override
     public Response randomAltFromText(String text1, String text2, Integer seed) {
+        if (!check.randomAltFromText(text1, text2, seed)) {
+            return check.getErrorResponse();
+        } 
         return Response.status(Status.NOT_IMPLEMENTED).build();
     }
 
     @Override
     public Response joinFromText(String text1, String text2, Integer col1, Integer col2) {
+        if (!check.joinFromText(text1, text2, col1, col2)) {
+            return check.getErrorResponse();
+        } 
         return Response.status(Status.NOT_IMPLEMENTED).build();
     }
 
     @Override
     public Response zipFromText(String type, String text1, String text2, String sep) {
+        if (!check.zipFromText(type, text1, text2, sep)) {
+            return check.getErrorResponse();
+        } 
         return Response.status(Status.NOT_IMPLEMENTED).build();
     }
 }
